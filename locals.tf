f089bd07e20ddc3b39bcd2867516283761ddd0fc
Onyedikachi <onyedikachinwosu@rocketmail.com>
2024-10-17 15:07:13 +0100

    initial commit

---
+++ locals.tf
@@ -0,0 +1,15 @@
+locals {
+    project = {
+        "sdm-project" = {
+            description = "Example description of Project"
+        }
+    }
+    workspace = {
+        "sdm-workspace" = {
+            description = "Example description of workspace"
+            execution_mode = "local"
+            project_id = module.project["sdm-project"].id
+            vcs_repo_identifier = "${var.github_organization_name}/sdm-terraform-tfe"
+        }
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "organization_name" {
+    default = "digital-factory"
+    type    = string
+}
+variable "github_app_installation_id" {
+    default = 56079337
+    type    = number
+}
+variable "github_organization_name" {
+    default= "Onyedikachi"
+    type    = string
+}   
