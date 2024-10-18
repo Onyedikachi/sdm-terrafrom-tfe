889fe3593ffa512a2322ab360e5bdeafa23f2971
Onyedikachi <onyedikachinwosu@rocketmail.com>
2024-10-18 07:20:31 +0100

    add github workspace

--- locals.tf
+++ locals.tf
@@ -11,5 +11,11 @@ locals {
             project_id = module.project["sdm-project"].id
             vcs_repo_identifier = "${var.github_organization_name}/sdm-terrafrom-tfe"
         }
+         "sdm-github" = {
+            description = "Example description of workspace"
+            execution_mode = "local"
+            project_id = module.project["sdm-project"].id
+            vcs_repo_identifier = "${var.github_organization_name}/sdm-terrafrom-github"
+        }
     }
 }
