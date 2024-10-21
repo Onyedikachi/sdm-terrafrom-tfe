abda7cfda2688e4a35fd111ab286ee2161a52a95
Nwosu Onyedikachi <onyedikachinwosu@rocketmail.com>
2024-10-21 16:07:45 +0100

    Add product service workspace (#5)
    * change sdm-github workspace to remote
    
    * add aws cluster and  netweork workspaces
    
    * add workspace

--- locals.tf
+++ locals.tf
@@ -71,5 +71,24 @@ locals {
         },
       ]
     }
+     "scm-product-service-prod" = {
+      description         = "Automation for product service resources."
+      execution_mode      = "remote"
+      project_id          = module.project["sdm-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/sdm-terraform-product-service"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "cluster_name"
+          value    = "sdm-kachi-prod"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+      ]
+    }
   }
 }
