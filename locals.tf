9fc1f5b2c39f87f038c940b7cec517975719c13a
Onyedikachi <onyedikachinwosu@rocketmail.com>
2024-10-22 11:05:05 +0100

    add the product service workspace

--- locals.tf
+++ locals.tf
@@ -17,5 +17,24 @@ locals {
       project_id          = module.project["sdm-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/sdm-terraform-github"
     }
+     "scm-product-service-prod" = {
+      description         = "Automation for product service resources."
+      execution_mode      = "remote"
+      project_id          = module.project["sdm-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/sdm-terraform-product-service"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "cluster_name"
+          value    = "sdm-kachi-prod"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+      ]
+    }
   }
 }
